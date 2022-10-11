d749f390e76049a11b1d43efaa6f5fcc026c6097
gioniemeyer <giovanna.niemeyer@gmail.com>
2022-10-10 22:47:38 -0300

    fix: adjust course module and exports

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
 @Controller('courses')
@@ -15,7 +15,13 @@ export class CoursesController {
   findOne(
     @Param('id') id: string
   ) {
-    return this.coursesService.findOne(id);
+    const course =  this.coursesService.findOne(id);
+
+    if (!course) {
+      throw new HttpException(`Course #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+
+    return course;    
   };
 
   @Post()
