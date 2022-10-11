af65dfd638f6c14d9d6e3c9d234fae504e2228d8
gioniemeyer <giovanna.niemeyer@gmail.com>
2022-10-10 22:09:50 -0300

    chore: create all courses paths

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
-@Controller()
+@Controller('courses')
 export class CoursesController {
-  @Get('courses')
-  findAll() {
-    return 'Listagem de cursos';
-  }
+  @Get()
+  findAll(
+    @Res() res
+  ) {
+    return res.status(200).send('Listagem de cursos');
+  };
+
+  @Get(':id')
+  findOne(
+    @Param('id') id: string
+  ) {
+    return `Curso #${id}`;
+  };
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(
+    @Body('name') body
+  ) {
+    return body;
+  };
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string, @Body() body
+  ) {
+    return `Atualização do curso #${id}`;
+  };
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Remoção do curso #${id}`;
+  };
 }
